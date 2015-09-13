a8e3ab249c65e5e093969f5d87dfe486427238da
John Dianala <jdianala@yahoo.com>
2015-09-13 17:58:28 -0500

    update

---
+++ Chess/Chess/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Program
+    {
+        class ChessBoard
+        {
+            Rows {get; set;}
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
